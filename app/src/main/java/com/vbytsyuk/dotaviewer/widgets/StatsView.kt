88b511627cbc01c03d2844e8e068266a9ec2096e
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-07-05 01:53:11 +0300

    Created StatsView class

---
+++ app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
@@ -0,0 +1,9 @@
+package com.vbytsyuk.dotaviewer.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class StatsView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+}
