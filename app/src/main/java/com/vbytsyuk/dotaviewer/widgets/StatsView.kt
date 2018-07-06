a51598dece8108347fe135dd6a549b847929f67f
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-07-06 10:16:50 +0300

    Add data class for StatsView

--- app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
+++ app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
@@ -6,4 +6,10 @@ import android.view.View
 
 class StatsView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : View(context, attrs, defStyleAttr, defStyleRes) {
 
+    data class Data(val avatarUrl: String,
+                    val name: String,
+                    val rank: String,
+                    val time: String,
+                    val winrate: String,
+                    val kda: String)
 }
