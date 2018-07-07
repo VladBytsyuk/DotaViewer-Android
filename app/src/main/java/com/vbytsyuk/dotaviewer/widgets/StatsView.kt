d341716630fd6cdadc2dcd59c1cd91a731d66a34
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-07-07 11:21:30 +0300

    Change StatsView.kt formating

--- app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
+++ app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
@@ -4,15 +4,17 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
-class StatsView
-        @JvmOverloads constructor(context: Context,
-                                  attrs: AttributeSet? = null,
-                                  defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
-
-    data class Data(val avatarUrl: String,
-                    val name: String,
-                    val rank: String,
-                    val time: String,
-                    val winrate: String,
-                    val kda: String)
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    data class Data(
+        val avatarUrl: String,
+        val name: String,
+        val rank: String,
+        val time: String,
+        val winrate: String,
+        val kda: String
+    )
 }
