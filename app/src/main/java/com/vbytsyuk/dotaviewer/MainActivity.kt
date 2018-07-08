71ba654970ad954286b7655a8f36cf6ab9882199
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-07-08 19:58:06 +0300

    Add test code into MainActivity

--- app/src/main/java/com/vbytsyuk/dotaviewer/MainActivity.kt
+++ app/src/main/java/com/vbytsyuk/dotaviewer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vbytsyuk.dotaviewer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.vbytsyuk.dotaviewer.widgets.StatsView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener { true }
+
+        statsView.setOnClickListener {
+            statsView.bind(
+                StatsView.Data(
+                    "",
+                    "Alan Turing",
+                    "Mathematician",
+                    "10000 hours",
+                    "69% winrate",
+                    "KDA: 42"
+                )
+            )
+        }
     }
 }
