4080fe0be1bb363bead8d2b70b3cca78c3dfcdf2
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-07-08 19:55:01 +0300

    Add textViews list to StatsView

--- app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
+++ app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
@@ -3,17 +3,22 @@ package com.vbytsyuk.dotaviewer.widgets
 import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
-import android.view.View
 import android.view.LayoutInflater
+import android.widget.TextView
+import kotlinx.android.synthetic.main.view_stats.view.*
+
 
 class StatsView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val textViews: List<TextView>
+
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         inflater.inflate(R.layout.view_stats, this, true)
+        textViews = listOf(name, rank, time, winrate, kda)
     }
     data class Data(
         val avatarUrl: String,
