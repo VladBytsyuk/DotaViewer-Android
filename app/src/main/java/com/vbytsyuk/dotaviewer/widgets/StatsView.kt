eb47bd6c7ebe83fb137c58e0a5bb2cca5156c0e2
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-07-08 19:54:09 +0300

    Change parent of StatsView to ConstraintLayout

--- app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
+++ app/src/main/java/com/vbytsyuk/dotaviewer/widgets/StatsView.kt
@@ -1,14 +1,20 @@
 package com.vbytsyuk.dotaviewer.widgets
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.View
+import android.view.LayoutInflater
 
 class StatsView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.view_stats, this, true)
+    }
     data class Data(
         val avatarUrl: String,
         val name: String,
